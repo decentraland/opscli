f84a353303835ddaa84f0a31e09f09cd264a71e4
Pedro Tamborindeguy <pftambo@gmail.com>
2023-03-03 15:23:31 -0500

    feat: concat-snapshots

---
+++ src/helpers/fileWriter.ts
@@ -0,0 +1,68 @@
+import * as fs from 'fs'
+import path from 'path'
+
+export type IFile = {
+  filePath: string
+  appendDebounced: (buffer: string) => Promise<void>
+  close: () => Promise<void>
+}
+
+async function checkFileExists(file: string): Promise<boolean> {
+  return fs.promises.access(file, fs.constants.F_OK).then(() => true).catch(() => false)
+}
+
+export async function createFileWriter(
+  // components: Pick<AppComponents, 'logs' | 'staticConfigs' | 'fs' | 'storage'>,
+  // filenamePrefix?: string
+  filename: string
+): Promise<IFile> {
+  const filePath = path.resolve(filename)
+
+  // if the process failed while creating the snapshot last time the file may still exists
+  // deleting the staging tmpFile just in case
+  if (await checkFileExists(filePath)) {
+    await fs.promises.unlink(filePath)
+  }
+
+  const file = fs.createWriteStream(filePath)
+
+  const fileClosedFuture = new Promise<void>((resolve, reject) => {
+    file.on('finish', resolve)
+    file.on('end', resolve)
+    file.on('error', reject)
+  })
+
+  // the following lines exist to naively emulate a buffering algorithm to reduce disk IO
+  // and thus, block the disk for less time using more memory
+  const MAX_WRITE_BUFFER_SIZE = 1000
+  const writeBuffer: Array<string> = []
+  async function flush() {
+    if (writeBuffer.length) {
+      const buffer = writeBuffer.join('')
+      writeBuffer.length = 0
+      await new Promise<void>((resolve, reject) => {
+        file.write(buffer, (err) => {
+          if (err) reject(err)
+          else resolve()
+        })
+      })
+    }
+  }
+
+  async function close() {
+    await flush()
+    file.close()
+    await fileClosedFuture
+  }
+
+  return {
+    filePath,
+    async appendDebounced(buffer: string) {
+      writeBuffer.push(buffer)
+      if (writeBuffer.length >= MAX_WRITE_BUFFER_SIZE) {
+        await flush()
+      }
+    },
+    close
+  }
+}
