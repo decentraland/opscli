e9f80aca16041cf39498855e5e552487eab444b0
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-08-08 12:22:00 -0300

    add --crossplatform to queue-ab-conversion-about for queing windows/mac/webgl

--- src/commands/queue-ab-conversion-about.ts
+++ src/commands/queue-ab-conversion-about.ts
@@ -1,7 +1,7 @@
 import { AuthLinkType } from "@dcl/schemas"
 import arg from "arg"
 import { assert } from "../helpers/assert"
-import { queueConversion } from "../helpers/asset-bundles"
+import { queueConversions } from "../helpers/asset-bundles"
 import { fetch } from "undici"
 import { CliError } from "../bin"
 import { parseEntityUrn } from "../helpers/parseEntityUrn"
@@ -12,6 +12,7 @@ export default async () => {
     "--ab-server": String,
     "--token": String,
     "--force": Boolean,
+    "--crossplatform": Boolean,
   })
 
   const aboutUrl = args["--about-url"]!
@@ -19,32 +20,48 @@ export default async () => {
   const abServer = args["--ab-server"] || "https://asset-bundle-converter.decentraland.org"
   const force = args["--force"] || false
 
+  const crossplatform = args["--crossplatform"] || false
+
+  const abServers = crossplatform
+    ? [
+        "https://asset-bundle-converter.decentraland.org",
+        "https://asset-bundle-converter-windows.decentraland.org",
+        "https://asset-bundle-converter-mac.decentraland.org",
+      ]
+    : [abServer]
+
   assert(!!token, "--token is missing")
 
   console.log(`>                 Parameters:`)
-  console.log(`         Asset bundle server: ${abServer}`)
+  console.log(`         Asset bundle server: ${JSON.stringify(abServers)}`)
   console.log(`               Force rebuild: ${force}`)
 
   const aboutReq = await fetch(aboutUrl)
   if (!aboutReq.ok) throw new CliError(`Invalid response from ${aboutUrl}`)
-  const aboutJson = await aboutReq.json() as any
+  const aboutJson = (await aboutReq.json()) as any
   if (!aboutJson.configurations.scenesUrn) throw new CliError(`Json has invalid format`)
 
   for (const urn of aboutJson.configurations.scenesUrn) {
     const parsed = parseEntityUrn(urn)
     console.log(`> Scheduling conversion of entity ${parsed.entityId}`)
-    const result = await queueConversion(abServer, {
-      entity: {
-        entityId: parsed.entityId, authChain: [
-          {
-            type: AuthLinkType.SIGNER,
-            payload: '0x0000000000000000000000000000000000000000',
-            signature: ''
-          }
-        ]
-      }, contentServerUrls: [parsed.baseUrl || "https://peer.decentraland.org/content"],
-      force
-    }, token)
+    const result = await queueConversions(
+      abServers,
+      {
+        entity: {
+          entityId: parsed.entityId,
+          authChain: [
+            {
+              type: AuthLinkType.SIGNER,
+              payload: "0x0000000000000000000000000000000000000000",
+              signature: "",
+            },
+          ],
+        },
+        contentServerUrls: [parsed.baseUrl || "https://peer.decentraland.org/content"],
+        force,
+      },
+      token
+    )
     console.log(`  Result: ${JSON.stringify(result)}`)
   }
 
--- src/helpers/asset-bundles.ts
+++ src/helpers/asset-bundles.ts
@@ -2,6 +2,14 @@ import { DeploymentToSqs } from "@dcl/schemas/dist/misc/deployments-to-sqs"
 import { fetch } from "undici"
 import { CliError } from "../bin"
 
+export async function queueConversions(assetConverterServers: string[], entity: DeploymentToSqs, token: string): Promise<Array<{ id: string }>> {
+  let ids: Array<{ id: string }> = []
+  for (const assetConverterServer of assetConverterServers) {
+    ids.push(await queueConversion(assetConverterServer, entity, token))
+  }
+  return ids
+}
+
 export async function queueConversion(assetConverterServer: string, entity: DeploymentToSqs, token: string): Promise<{ id: string }> {
   const url = `${assetConverterServer}/queue-task`
   const res = await fetch(url, {
