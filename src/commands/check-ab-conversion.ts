bd7ee774417e945f060555caf4696cd9128e192b
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2023-12-20 12:19:58 -0300

    Added check for platform in ab conversion

--- src/commands/check-ab-conversion.ts
+++ src/commands/check-ab-conversion.ts
@@ -10,12 +10,14 @@ export default async () => {
     "--pointer": [String],
     "--content-server": String,
     "--ab-cdn": String,
+    "--ab-platform": String,
   })
 
   const pointers = args["--pointer"] || []
   const cids = args["--cid"] || []
   const abServer = args["--ab-cdn"] || "https://ab-cdn.decentraland.org"
-
+  var abPlatform = args["--ab-platform"]
+  
   assert(pointers.length > 0 || cids.length > 0, "--pointer or --cid are required")
 
   const contentUrl = (args["--content-server"] || "https://peer.decentraland.org/content").replace(/\/$/, "")
@@ -36,19 +38,33 @@ export default async () => {
     }
   }
 
+  let displayPlatform = 'WebGL'; // Default displayPlatform to 'WebGL'
+
+  // Check if abPlatform is either 'windows' or 'mac'
+  if (abPlatform) {
+    if (abPlatform === 'windows' || abPlatform === 'mac') {
+      displayPlatform = abPlatform; // Set displayPlatform to abPlatform if it's valid
+      abPlatform = `_${abPlatform}`;
+    } else {
+      console.log(`! Invalid Platform: ${abPlatform}. Defaulting to WebGL`);
+      abPlatform = '';
+    }
+  }
+
   for (const { entityId, pointers } of entityIdsToConvert) {
-    const result = await fetch(`${abServer}/manifest/${entityId}.json`)
+    // Construct the URL dynamically based on abPlatform
+    const result = await fetch(`${abServer}/manifest/${entityId}${abPlatform}.json`);
     if (!result.ok) {
       const failManifest = await fetch(`${abServer}/manifest/${entityId}_failed.json`)
       if (failManifest.ok) {
         const manifest = await failManifest.json() as any
-        console.log(`🟠 ${entityId} (${pointers[0]}): Failed. Version=${manifest.version} ExitCode=${manifest.exitCode} Date=${manifest.date} Log=${manifest.log}`)
+        console.log(`🟠 ${entityId} (${pointers[0]}): Failed. Version=${manifest.version} ExitCode=${manifest.exitCode} Date=${manifest.date} Log=${manifest.log} Platform=${displayPlatform}`)
       } else {
         console.log(`🔴 ${entityId} (${pointers[0]}): Not converted!`)
       }
     } else {
       const manifest = await result.json() as any
-      console.log(`🟢 ${entityId} (${pointers[0]}): Version=${manifest.version} ExitCode=${manifest.exitCode} Date=${manifest.date}`)
+      console.log(`🟢 ${entityId} (${pointers[0]}): Version=${manifest.version} ExitCode=${manifest.exitCode} Date=${manifest.date} Platform=${displayPlatform}`)
     }
   }
 
